33f50aa58474948aa1d848b37d10bc3cdcc69a9e
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-07-21 00:42:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+import GlobalState from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GlobalState>
+  <ChakraProvider theme={theme}>
+    <App/>
+  </ChakraProvider>
+  </GlobalState>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    intialColorMode : 'light',
+    useSystemColorMode: true
+})
+
+export default theme;
