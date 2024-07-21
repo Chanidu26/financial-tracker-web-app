9fe548bcca6f90519003dc2056d27f3e494b68eb
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-07-22 00:03:35 +0530

    added gh-pages configurations

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Main from './components/main';
 
 function App() {
   return (
-    <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
-      <Flex height={"full"}>
-        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
-          <Main />
-        </Box>
-      </Flex>
+    <Container bg={'#f8fafd'} maxW={'container.3xl'} height={'100vh'} p={('0')}>
+        
+        <Flex height = {"full"}>
+          <Box height = {"full"} flex = {5} w = {['20%','30%','20%','50%','60%']}>
+            
+            <Main/>
+          </Box>
+        </Flex>
     </Container>
   );
 }
